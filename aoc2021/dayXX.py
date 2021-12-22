bbb5fd09c9cff70fdf68910907b93a53e62f2ba3
Antoine Beyeler <abeyeler@ab-ware.com>
2021-12-22 11:15:26 +0100

    pytest-based, better, cleaner template with execution time

--- aoc2021/dayXX.py
+++ aoc2021/dayXX.py
@@ -1,19 +1,21 @@
-import sys
+import time
 from pathlib import Path
-from typing import Callable
 
 import aocd
+import pytest
 from rich.console import Console
 from rich.traceback import install
 
 install(show_locals=True)
 console = Console()
 
+YEAR = 2021
 DAY = int("".join(s for s in Path(__file__).name if s.isdigit()))
 # noinspection SpellCheckingInspection
 TEST_DATA: str = """"""
 TEST_RESULT_PART1: int | None = None
 TEST_RESULT_PART2: int | None = None
+DATA = aocd.get_data(day=DAY, year=2021)
 
 
 def part1(data: str) -> int:
@@ -24,43 +26,28 @@ def part2(data: str) -> int:
     pass
 
 
-def run_test_part1(*args, **kwargs):
-    if TEST_RESULT_PART1 is None:
-        raise NotImplementedError
-    # noinspection PyArgumentList
-    assert part1(TEST_DATA, *args, **kwargs) == TEST_RESULT_PART1
+@pytest.mark.skipif(TEST_RESULT_PART1 is None, reason="part 1 test result not provided")
+def test_part1():
+    assert part1(TEST_DATA) == TEST_RESULT_PART1
 
 
-def run_test_part2(*args, **kwargs):
-    if TEST_RESULT_PART2 is None:
-        raise NotImplementedError
-    # noinspection PyArgumentList
-    assert part2(TEST_DATA, *args, **kwargs) == TEST_RESULT_PART2
-
-
-def run_solution(name: str, test_func: Callable, solution: Callable, *args, **kwargs):
-    proceed = True
-    try:
-        test_func(*args, **kwargs)
-        console.print(f"Test {name} succeeded.", style="green")
-    except NotImplementedError:
-        console.print(f"Test {name} not implemented", style="yellow")
-        proceed = console.input("Proceed? ").lower() in ["y", "yes"]
-    except AssertionError:
-        console.print(f"Test {name} failed", style="red")
-        proceed = console.input("Proceed? ").lower() in ["y", "yes"]
-
-    if not proceed:
-        sys.exit()
-
-    result = solution(aocd.get_data(day=DAY, year=2021), *args, **kwargs)
-    console.print(f"{name.capitalize()} solution: {result}", style="blue")
+@pytest.mark.skipif(TEST_RESULT_PART2 is None, reason="part 2 test result not provided")
+def test_part2():
+    assert part2(TEST_DATA) == TEST_RESULT_PART2
 
 
 def main() -> None:
-    console.print(f"Running for day {DAY}", style="blue")
-    run_solution("part 1", run_test_part1, part1)
-    run_solution("part 2", run_test_part2, part2)
+    console.print(f"AOC {YEAR} day {DAY}", style="blue")
+    console.print("Tests: ", end="", style="blue")
+    pytest.main(["-q", __file__])
+    start = time.time()
+    res = part1(DATA)
+    delta = time.time() - start
+    console.print(f"Part 1 solution: {res} (execution time: {delta*1000:.2f}ms)", style="blue")
+    start = time.time()
+    res = part2(DATA)
+    delta = time.time() - start
+    console.print(f"Part 2 solution: {res} (execution time: {delta*1000:.2f}ms)", style="blue")
 
 
 if __name__ == "__main__":
